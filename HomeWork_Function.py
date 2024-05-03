6350f3a333c8df14a86c2a0ad1225170908919cd
KonstantinKatkov <167233421+KonstantinKatkov@users.noreply.github.com>
2024-05-03 12:14:18 +0300

    Add files via upload
    Домашнее задание. Функции

---
+++ HomeWork_Function.py
@@ -0,0 +1,13 @@
+def print_params(param):
+    print("Параметр функции", param)
+    print("Параметр функции", param)
+
+print("Первый вызов функции")
+print_params("Привет")
+
+print("Второй вызов функции")
+print_params("Как дела?")
+
+print("Третий вызов функции")
+print_params(2024)
+
